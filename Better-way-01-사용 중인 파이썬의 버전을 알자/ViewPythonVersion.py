49a949941d3fc31e01b77f455f18ed27be90cc61
SHGroup <sunghun7511@naver.com>
2018-03-15 15:13:20 +0900

    Better-way-01

---
+++ Better-way-01-사용 중인 파이썬의 버전을 알자/ViewPythonVersion.py
@@ -0,0 +1,16 @@
+import sys
+
+print(sys.version_info)
+print(sys.version)
+
+'''
+shgroup@SH-Group:/mnt/d/Dev/Python/Effective-Python/Better-way-01$ python ViewPythonVersion.py
+sys.version_info(major=2, minor=7, micro=12, releaselevel='final', serial=0)
+2.7.12 (default, Nov 20 2017, 18:23:56)
+[GCC 5.4.0 20160609]
+shgroup@SH-Group:/mnt/d/Dev/Python/Effective-Python/Better-way-01$ python3 ViewPythonVersion.py
+sys.version_info(major=3, minor=5, micro=2, releaselevel='final', serial=0)
+3.5.2 (default, Nov 23 2017, 16:37:01)
+[GCC 5.4.0 20160609]
+shgroup@SH-Group:/mnt/d/Dev/Python/Effective-Python/Better-way-01$
+'''
